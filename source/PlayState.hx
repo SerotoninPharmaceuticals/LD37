51811ee7a88e71d85b84cc012deae78fbf6b7fe1
houkanshan <houkanshan@gmail.com>
2016-12-10 16:59:36 +0800

    reformat code.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,21 +3,19 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 
-class PlayState extends FlxState
-{
+class PlayState extends FlxState {
   var isPaused = true;
+  var currentDay = 0;
 
-	override public function create():Void
-	{
+  override public function create():Void {
     FlxG.mouse.useSystemCursor = true;
     super.create();
     GameData.load();
     FlxG.log.add(GameData.data.elapsed);
-	}
+  }
 
-	override public function update(elapsed:Float):Void
-	{
-		super.update(elapsed);
+  override public function update(elapsed:Float):Void {
+    super.update(elapsed);
 
     var totalElapsed = GameData.data.elapsed;
     totalElapsed += elapsed;
@@ -27,11 +25,12 @@ class PlayState extends FlxState
     FlxG.log.add(getElapsedToday(totalElapsed));
 
     GameData.save();
-	}
+  }
 
   function getElapsedToday(totalElapsed:Float):Float {
     return totalElapsed % GameConfig.elapsedEachDay;
   }
+
   function getElapsedDays(totalElapsed:Float):Float {
     return Math.floor(totalElapsed / GameConfig.elapsedEachDay);
   }
