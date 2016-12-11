5a6705bb00863c9a9efc5e1f5e69a4cfcbffbc74
abing <ohabing@gmail.com>
2016-12-11 23:47:00 +0800

    add newspaper sound

--- source/sprites/NewsReader.hx
+++ source/sprites/NewsReader.hx
@@ -1,13 +1,22 @@
 package sprites;
 
+import flixel.FlxG;
 import flixel.util.FlxSpriteUtil;
 import flixel.math.FlxRandom;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.util.FlxTimer;
+import flixel.system.FlxSound;
+
 class NewsReader extends FlxTypedGroup<LanguageGenerator> {
   var title:LanguageGenerator;
   var body:LanguageGenerator;
   var rand = new FlxRandom();
+  var paperSound:FlxSound;
+
+  public function new() {
+    super();
+    paperSound = FlxG.sound.load("assets/sounds/paper.wav");
+  }
 
   public function showNews() {
     if (body != null) { return; }
@@ -20,6 +29,7 @@ class NewsReader extends FlxTypedGroup<LanguageGenerator> {
     add(title);
     add(body);
 
+    paperSound.play();
     var timer = new FlxTimer();
     timer.start(5, function(t) {
       var fadeOutDuration = 1;
