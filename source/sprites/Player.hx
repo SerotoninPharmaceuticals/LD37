74b61072ec990004c51f9522e2ede882042a06a3
serotoninphobe <gray_hawks@hotmail.com>
2016-12-12 16:12:59 +0800

    修正动画状态设置

--- source/sprites/Player.hx
+++ source/sprites/Player.hx
@@ -33,8 +33,8 @@ class Player extends FlxSprite {
 
     loadGraphic("assets/images/animation_player.png", true, 22, 22);
     animation.add("walk", [0, 1, 2, 3, 4, 5], 5, false);
-    animation.add("eat_and_drink", [6, 7, 8, 9, 10, 11], 5, false);
-    animation.add("read_and_toilet", [12, 13, 14, 15, 16, 17], 5, false);
+    animation.add("eat_and_drink", [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], 5, false);
+    animation.add("read_and_toilet", [6, 7, 8, 15, 16, 17], 5, false);
     drag.x = drag.y = 1600;
     setSize(8, 14);
     offset.set(4, 2);
